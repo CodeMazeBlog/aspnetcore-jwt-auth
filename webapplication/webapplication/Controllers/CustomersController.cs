8c2190c9ef3009cef2e6e9fc2e2b57e27942ef42
Marinko <marinkospasojevic@gmail.com>
2019-02-19 13:10:05 +0100

    support for 2.2 .NET Core

---
+++ webapplication/webapplication/Controllers/CustomersController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace webapplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet, Authorize(Roles = "Manager")]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "John Doe", "Jane Doe" };
+        }
+    }
+}
---
+++ webapplication/webapplication/Models/LoginModel.cs
@@ -0,0 +1,4 @@
+public class LoginModel{
+    public string UserName { get; set; }
+    public string Password { get; set; }
+}
