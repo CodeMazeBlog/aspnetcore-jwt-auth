f60bae647d6709236adc62f41033c3d5aa96932c
Marinko <marinkospasojevic@gmail.com>
2019-11-01 18:02:32 +0100

    ASP.NET Core 3.0 support

--- webapplication/Models/LoginModel.cs
+++
@@ -1,4 +0,0 @@
-public class LoginModel{
-    public string UserName { get; set; }
-    public string Password { get; set; }
-}
---
+++ webapplication/webapplication/Models/LoginModel.cs
@@ -0,0 +1,8 @@
+﻿namespace webapplication.Models
+{
+    public class LoginModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
